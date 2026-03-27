d172ee52c173eb9ce252dd3d6a9440cf476f3625
RevengeZ ⌁ <lattapon.mu@gmail.com>
2026-03-27 16:34:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -235,8 +235,7 @@ if file1:
 
     with pd.ExcelWriter(output, engine='openpyxl') as writer:
 
-        df_vin1.to_excel(writer, index=False, sheet_name='TCAPLinkageDatahub
-')
+        df_vin1.to_excel(writer, index=False, sheet_name='VIN_FULL')
 
         if not df_vin2.empty:
             df_vin2.to_excel(writer, index=False, sheet_name='TCAPLinkage')
